42a5365ee4cf38ad1ffcc2f12571e64542ca371a
csyszf <csyszf@outlook.com>
2017-09-05 18:20:32 +0800

    bootstrap

---
+++ src/JF.Identity.Grain.Abstractions/IUserGrain.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Threading.Tasks;
+using Orleans;
+
+namespace JF.Identity.Grain.Abstractions
+{
+    public interface IUserGrain: IGrainWithGuidKey
+    {
+        Task<string> SignUpAsync(string email);
+    }
+}
---
+++ src/JF.Identity.Grain/UserGrain.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading.Tasks;
+using JF.Identity.Grain.Abstractions;
+using Orleans;
+
+namespace JF.Identity.Grain
+{
+    public class UserGrain: Orleans.Grain, IUserGrain
+    {
+        public Task<string> SignUpAsync(string email)
+        {
+            return Task.FromResult("success");
+        }
+    }
+}
