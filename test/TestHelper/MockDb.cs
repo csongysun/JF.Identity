853af483cb5b00e79a54e3efadfbd077b78f5d3e
csyszf <csyszf@outlook.com>
2017-09-15 14:36:18 +0800

    add testhelper

---
+++ test/TestHelper/MockDb.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using JF.Identity.Grain;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace TestHelper
+{
+    public static class MockDb
+    {
+        private static IdentityContext _context;
+        public static IdentityContext Sqlite
+        {
+            get
+            {
+                if (_context is null)
+                {
+                    var lf = new LoggerFactory();
+                    lf.AddProvider(new SqlLoggerProvider());
+                    var connection = new SqliteConnection("DataSource=:memory:");
+                    connection.Open();
+                    var options = new DbContextOptionsBuilder<IdentityContext>()
+                                        .UseSqlite(connection)
+                                        .UseLoggerFactory(lf)
+                                        .Options;
+
+                    var context = new IdentityContext(options);
+                    context.Database.EnsureCreated();
+                    _context = context;
+                }
+
+                return _context;
+            }
+        }
+    }
+}
---
+++ test/TestHelper/SqlLoggerProvider.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Extensions.Logging;
+
+namespace TestHelper
+{
+    public class SqlLoggerProvider : ILoggerProvider
+    {
+        public ILogger CreateLogger(string categoryName)
+        {
+            return new NullLogger();
+        }
+
+        public void Dispose()
+        { }
+
+        private class SqlLogger : ILogger
+        {
+            private string _test;
+
+            public SqlLogger(string test)
+            {
+                _test = test;
+            }
+
+            public bool IsEnabled(LogLevel logLevel)
+            {
+                return true;
+            }
+
+            public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter)
+            {
+            }
+
+            public IDisposable BeginScope<TState>(TState state)
+            {
+                return null;
+            }
+        }
+
+        private class NullLogger : ILogger
+        {
+            public bool IsEnabled(LogLevel logLevel)
+            {
+                return false;
+            }
+
+            public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter)
+            { }
+
+            public IDisposable BeginScope<TState>(TState state)
+            {
+                return null;
+            }
+        }
+    }
+}
