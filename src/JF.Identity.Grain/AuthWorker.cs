03a52e53f0d32b579c7a325dc1892875246c430d
csyszf <csyszf@outlook.com>
2017-09-18 17:02:19 +0800

    restore

--- src/JF.Identity.Grain/AuthWorker.cs
+++ src/JF.Identity.Grain/AuthWorker.cs
@@ -27,12 +27,8 @@ namespace JF.Identity.Grain
             await _context.AddAsync(user);
             await _context.SaveChangesAsync();
 
-            //var userGrain = GrainFactory.GetGrain<IUserGrain>(user.Id);
-
-            ////Task.Run(() => userGrain.BeginSignUpAsync(cmd.PasswordHash)).Start();
-
-            ////userGrain.InvokeOneWay(_ => _.BeginSignUpAsync(cmd.Password));
-            //await userGrain.BeginSignUpAsync(cmd.Password);
+            var userGrain = GrainFactory.GetGrain<IUserGrain>(user.Id);
+            userGrain.InvokeOneWay(_ => _.BeginSignUpAsync(cmd.Password));
             return CommandResult.Ok;
         }
 
