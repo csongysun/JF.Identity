3dff1027be73a9a85b5087997e014a1565a7d5f5
csyszf <csyszf@outlook.com>
2017-09-22 10:31:12 +0800

    remove integration tests TestHost not work on .net core

--- test/JF.Identity.IntegrationTests/ClusterFixture.cs
+++ test/JF.Identity.IntegrationTests/ClusterFixture.cs
@@ -1,27 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using Orleans.TestingHost;
-using Orleans.Runtime.Configuration;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+//using Orleans.TestingHost;
+//using Orleans.Runtime.Configuration;
 
-namespace JF.Identity.IntegrationTests
-{
-    public class ClusterFixture: IDisposable
-    {
-        public TestCluster Cluster { get; private set; }
+//namespace JF.Identity.IntegrationTests
+//{
+//    public class ClusterFixture: IDisposable
+//    {
+//        public TestCluster Cluster { get; private set; }
 
-        public ClusterFixture()
-        {
-            var config = new ClusterConfiguration();
-            config.UseStartupType<TestStartup>();
-            this.Cluster = new TestCluster();
-            this.Cluster.Deploy();
-        }
+//        public ClusterFixture()
+//        {
+//            var config = new ClusterConfiguration();
+//            config.UseStartupType<TestStartup>();
+//            this.Cluster = new TestCluster();
+//            this.Cluster.Deploy();
+//        }
 
-        public void Dispose()
-        {
-            this.Cluster.StopAllSilos();
-        }
+//        public void Dispose()
+//        {
+//            this.Cluster.StopAllSilos();
+//        }
 
-    }
-}
+//    }
+//}
--- test/JF.Identity.IntegrationTests/TestEndPoint.cs
+++ test/JF.Identity.IntegrationTests/TestEndPoint.cs
@@ -1,18 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using Orleans.TestingHost;
-using Xunit;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+//using Orleans.TestingHost;
+//using Xunit;
 
-namespace JF.Identity.IntegrationTests
-{
-    public class TestEndPoint: IClassFixture<ClusterFixture>
-    {
-        protected readonly TestCluster Cluster;
+//namespace JF.Identity.IntegrationTests
+//{
+//    public class TestEndPoint: IClassFixture<ClusterFixture>
+//    {
+//        protected readonly TestCluster Cluster;
 
-        public TestEndPoint(ClusterFixture fixture)
-        {
-            Cluster = fixture.Cluster;
-        }
-    }
-}
+//        public TestEndPoint(ClusterFixture fixture)
+//        {
+//            Cluster = fixture.Cluster;
+//        }
+//    }
+//}
